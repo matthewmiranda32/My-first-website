706b837a503406cfb96994e7b8e551094ffa07ad
Matthew Miranda <matthewmiranda32@gmail.com>
2017-08-25 08:05:45 -0500

    Corrected Files

--- Dice-Game-Project/getbacktowhereyouoncebelong.js
+++ Dice-Game-Project/getbacktowhereyouoncebelong.js
@@ -1,12 +1,13 @@
 "use strict"
 
-function twelveRoll(){
+function rollForHome(){
 	var roll = Math.floor((Math.random()*20)+1);
 
-	if (roll<=17){
+	if(roll<=17){
 		window.location.assign("universeracehome.html");
 	}
 	else {
 		document.write("Try Again");
 	}
+}
 
